c9c87585fbc1a32bb883a91fed434ffb2e835bf3
laylla986 <lbss7@aluno.ifal.edu.br>
2024-12-08 20:10:46 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let numero = ("digite um numero:"), 10);
+
+if (numero == numero) {
+    alert("tabuada de multiplicao do numero " + numero + ":");
+
+  
+    for (var i = 1; i <= 10; i++) {
+        alert(numero + " x " + i + " = " + (numero * i));
+    }
+
+} else {
+
+    alert("por favor, insira um numero valido.");
+    
+}
